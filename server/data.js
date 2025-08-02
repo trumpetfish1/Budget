17749c1ae61b0847eb2b357f92e7de5bad731c68
trumpetfish1 <trumpetfish1@gmail.com>
2025-08-02 14:57:07 -0400

    Format objects

--- server/data.js
+++ server/data.js
@@ -1,6 +1,6 @@
-const envelopes = [ {
-    envelope: 'checking', amount: 0},
-    {envelope: 'savings', amount: 0
-} ]
+const envelopes = [ 
+    {envelope: 'checking', amount: 0},
+    {envelope: 'savings', amount: 0},
+        {envelope: 'vacation', amount: 0} ]
 
 module.exports = { envelopes }
