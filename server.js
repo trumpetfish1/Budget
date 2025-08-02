110fe0738c3f24120be25cd9ed4134d08d85ff44
trumpetfish1 <trumpetfish1@gmail.com>
2025-08-01 20:10:59 -0400

    Add packages

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+const PORT = 4001;
+
+// Add middleware for handling CORS requests from index.html
+const cors = require('cors');
+app.use(cors());
+
+// Add middware for parsing request bodies here:
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Mount your existing apiRouter below at the '/api' path.
+const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
+
+// Add your code to start the server listening at PORT below:
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
